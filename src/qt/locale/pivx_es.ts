cea7bd57555fd5fd8d1c18e3e8ad03ffe48c339f
Kopernikus-dev <kpoernikus-dev@thomasrohde.eu>
2020-11-28 20:50:45 +0100

    update giturl

--- src/qt/locale/pivx_es.ts
+++ src/qt/locale/pivx_es.ts
@@ -1734,9 +1734,9 @@ Are you sure?</source>
     </message>
     <message>
         <source>Language missing or translation incomplete? Help contributing translations here:
-https://www.transifex.com/pivx-project/pivx-project-translations</source>
+https://www.transifex.com/kopernikus-dev/pivx4.2-project-translations</source>
         <translation>¿Falta su lenguaje o la traducción está incompleta? Contribuya con las traducciones aquí:
-https://www.transifex.com/pivx-project/pivx-project-translations</translation>
+https://www.transifex.com/kopernikus-dev/pivx4.2-project-translations</translation>
     </message>
     <message>
         <source>Map port using &amp;UPnP</source>
--- src/qt/locale/pivx_fr_FR.ts
+++ src/qt/locale/pivx_fr_FR.ts
@@ -1887,9 +1887,9 @@ STAKING</source>
     </message>
     <message>
         <source>Language missing or translation incomplete? Help contributing translations here:
-https://www.transifex.com/pivx-project/pivx-project-translations</source>
+https://www.transifex.com/kopernikus-dev/pivx4.2-project-translations</source>
         <translation>Langue manquante ou traduction incomplète? Aidez-nous à traduire ce projet ici:
-https://www.transifex.com/pivx-project/pivx-project-translations</translation>
+https://www.transifex.com/kopernikus-dev/pivx4.2-project-translations</translation>
     </message>
     <message>
         <source>Map port using &amp;UPnP</source>
